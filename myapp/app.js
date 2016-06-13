7f151cee3486f510e6a8e30d9669175edcfe96b1
mojo <realmojo88@gmail.com>
2016-06-13 19:07:51 +0900

    commit_3

--- myapp/app.js
+++ myapp/app.js
@@ -1,14 +1,63 @@
 var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
-app.use(express.static(__dirname+'/public'));
-console.log(__dirname);
-/*
-app.get('/',function(req, res){
-	res.send("Hello World!");
+mongoose.connect(process.env.MONG_DB);
+var db = mongoose.connection;
+
+db.once('open',function(){
+	console.log("DB Connected");
 });
-*/
+
+db.once('error',function(err){
+	console.log("DB Error : "+err);
+});
+
+var dataSchema = mongoose.Schema({
+	name:String,count:Number
+});
+
+var Data = mongoose.model('data',dataSchema);
+
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+console.log(__dirname);
+
+var data = {count:0};
+app.get('/',function(req, res){
+	//res.send("Hello World!");
+	Data.findOne({name:"myData"}, function(err,data){
+		if(err){return console.log('Data error : '+err);}
+		data.count++;
+		data.save(function(err){
+			if(err){
+				return console.log("error");
+			}
+			res.render('my_first_ejs',data);
+		});
+	});
+});
+
+app.get('/reset',function(req,res){
+	data.count = 0;
+	res.render('my_first_ejs',data);
+});
+
+app.get('/set/count',function(req,res){
+	if(req.query.count){
+		data.count = req.query.count;
+	}
+	res.render('my_first_ejs',data);
+});
+
+app.get('/set/:num',function(req,res){
+	data.count = req.params.num;
+	res.render('my_first_ejs',data);
+});
+
 
 app.listen(80, function(){
 	console.log('server On');
